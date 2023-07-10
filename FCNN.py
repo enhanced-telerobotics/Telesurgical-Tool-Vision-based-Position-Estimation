80332b3d8450f0f303c23ea5f3a47a920ee5278b
Shuyuan Yang <sxy841@case.edu>
2023-07-10 12:50:40 -0400

    save training info to loss file

--- FCNN.py
+++ FCNN.py
@@ -3,17 +3,29 @@ from torch.utils.data import TensorDataset, DataLoader
 import torch.nn as nn
 import torch.optim as optim
 import numpy as np
+import json
+from datetime import datetime
 
 
 class TwoInputFCNN:
-    def __init__(self, input_dim, hidden_dim, output_dim, device, num_hidden_layers=2, lr=0.001):
+    def __init__(self, input_dim, hidden_dim, output_dim, device, num_hidden_layers=2, lr=0.001, batch_size=32):
         self.device = device
         self.lr = lr
         self.num_hidden_layers = num_hidden_layers
+        self.batch_size = batch_size
         self.losses = []
+        self.params = {'input_dim': input_dim,
+                       'hidden_dim': hidden_dim,
+                       'output_dim': output_dim,
+                       'device': str(device),
+                       'num_hidden_layers': num_hidden_layers,
+                       'lr': lr,
+                       'batch_size': batch_size}
 
-        self.model_R = self._create_hidden_layers(input_dim, hidden_dim, num_hidden_layers).to(self.device)
-        self.model_L = self._create_hidden_layers(input_dim, hidden_dim, num_hidden_layers).to(self.device)
+        self.model_R = self._create_hidden_layers(
+            input_dim, hidden_dim, num_hidden_layers).to(self.device)
+        self.model_L = self._create_hidden_layers(
+            input_dim, hidden_dim, num_hidden_layers).to(self.device)
 
         self.fc = nn.Linear(hidden_dim * 2, output_dim).to(self.device)
 
@@ -29,20 +41,21 @@ class TwoInputFCNN:
     def parameters(self):
         return list(self.model_R.parameters()) + list(self.model_L.parameters()) + list(self.fc.parameters())
 
-    def train(self, X_R, X_L, y, epochs=100, batch_size=32, use_tqdm=True, save_loss=False):
+    def train(self, X_R, X_L, y, epochs=100, use_tqdm=True, save_loss=False):
         X_R_tensor = torch.tensor(X_R, dtype=torch.float32).to(self.device)
         X_L_tensor = torch.tensor(X_L, dtype=torch.float32).to(self.device)
         y_tensor = torch.tensor(y, dtype=torch.float32).to(self.device)
 
         dataset = TensorDataset(X_R_tensor, X_L_tensor, y_tensor)
-        data_loader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+        data_loader = DataLoader(
+            dataset, batch_size=self.batch_size, shuffle=True)
 
         if use_tqdm:
             from tqdm import tqdm
             pbar = tqdm(total=epochs*len(data_loader),
                         desc="Training Progress")
             step = 0
-        
+
         for epoch in range(epochs):
             for X_R_batch, X_L_batch, y_batch in data_loader:
                 self.optimizer.zero_grad()
@@ -58,7 +71,7 @@ class TwoInputFCNN:
                     pbar.set_description(f"Epoch {epoch}")
                     pbar.update()
                     step += 1
-            
+
             if use_tqdm:
                 pbar.set_postfix({'loss': loss.item()})
 
@@ -69,7 +82,16 @@ class TwoInputFCNN:
             pbar.close()
 
         if save_loss:
-            np.save('losses.npy', np.array(self.losses))
+            self.losses.append((epoch, loss.item()))
+            self.params['losses'] = self.losses
+
+            # Get the current time and format it as a string
+            now = datetime.now()
+            timestamp = now.strftime("%Y%m%d_%H%M%S")
+
+            # Save the parameters and losses with a timestamp in the filename
+            with open(f'params_and_losses_{timestamp}.json', 'w') as f:
+                json.dump(self.params, f)
 
     def predict(self, X_R, X_L):
         X_R_tensor = torch.tensor(X_R, dtype=torch.float32).to(self.device)
